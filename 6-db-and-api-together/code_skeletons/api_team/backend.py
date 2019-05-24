5e03760b78d54a87df2c1c348076e24022d07df9
bradleyjay <bradleyjshields@gmail.com>
2019-05-23 21:48:22 -0400

    Shelbs and Siobhan and Brad GREAT SUCCESS

---
+++ 6-db-and-api-together/code_skeletons/api_team/backend.py
@@ -0,0 +1,93 @@
+import requests
+import time
+import datetime
+
+class Weather_obj:
+
+    def __init__(self, time, summary, temperature, precipitation_intensity, precipitation_type, precipitation_probability):
+            self.time = time
+            self.summary = summary
+            self.temperature = temperature
+            self.precipitation_intensity = precipitation_intensity
+            self.precipitation_type = precipitation_type
+            self.precipitation_probability = precipitation_probability
+
+
+def make_connection():
+    resp = requests.get('https://api.darksky.net/forecast/c49ab0cc157f5ad35bdb1a9b0769853e/40.756438,-73.990299')
+    data = resp.json()
+    return data
+
+def parse_current(data):
+    currently = data["currently"]
+    
+    # object Weather_obj requires precipType
+    if "precipType" not in currently:
+        currently["precipType"] = "null"
+
+    parsed = create_model(
+        currently["time"], 
+        currently["summary"], 
+        currently["temperature"], 
+        currently["precipIntensity"], 
+        currently["precipType"], 
+        currently["precipProbability"]
+        )
+    
+    crud(parsed)
+
+    # jankny test
+    print(parsed)
+
+def parse_future(data):
+    hourly = data["hourly"]["data"]
+    
+  
+    model_array = []
+
+    for i in hourly:
+
+        # object Weather_obj requires precipType
+        if "precipType" not in i:
+            parsed["precipType"] = "null"
+
+        # this preciptType is busted, fix it in both!!
+        parsed = create_model(
+            i["time"], 
+            i["summary"], 
+            i["temperature"], 
+            i["precipIntensity"], 
+            i["precipType"], 
+            i["precipProbability"]
+            )
+        
+        model_array.insert(0,parsed)
+
+
+    crud(model_array)
+
+
+def create_model(raw):
+    inst = Weather_obj(raw.time, raw.summary, raw.temperature, raw.precipitation_intensity,  raw.precipitation_type, raw.precipitation_probability)
+    return inst
+
+def crud(an_input):
+    # call the DB team's crud function here
+    pass
+
+while True:
+
+    data = make_connection()
+    
+    if datetime.datetime.now().time().hour == 0 and datetime.datetime.now().time().minute == 0:
+        
+        parse_future(data)
+    
+    parse_current(data)
+    
+    # wait one minute
+    time.sleep(60)
+
+
+
+
