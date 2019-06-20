60fdd106225690275570b278ad47d29c37d5ab45
mlm603 <meghan.maloy@datadoghq.com>
2019-06-20 18:40:20 -0400

    working files

---
+++ projects/weathervane/server/db/model.py
@@ -0,0 +1,9 @@
+class Weather_obj:
+
+    def __init__(self, time, summary, temperature, precipitation_intensity, precipitation_type, precipitation_probability):
+            self.time = time
+            self.summary = summary
+            self.temperature = temperature
+            self.precipitation_intensity = precipitation_intensity
+            self.precipitation_type = precipitation_type
+            self.precipitation_probability = precipitation_probability
