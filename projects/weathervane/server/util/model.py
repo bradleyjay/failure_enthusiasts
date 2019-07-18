d974d4f27ff684b837bc5ba5e68990436161f238
bradleyjay <bradleyjshields@gmail.com>
2019-07-18 18:53:24 -0400

    Mass fixes - new architecture.

--- projects/weathervane/server/util/model.py
+++
@@ -1,9 +0,0 @@
-class Weather_obj:
-
-    def __init__(self, time, summary, temperature, precipitation_intensity, precipitation_type, precipitation_probability):
-            self.time = time
-            self.summary = summary
-            self.temperature = temperature
-            self.precipitation_intensity = precipitation_intensity
-            self.precipitation_type = precipitation_type
-            self.precipitation_probability = precipitation_probability
