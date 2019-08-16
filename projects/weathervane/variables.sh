5f06de3ac8925ab3c41316e21bab5f090173611b
soitbros <clint.osterholz@datadoghq.com>
2019-08-15 20:41:27 -0400

    gitignore can die2

--- projects/weathervane/variables.sh
+++ projects/weathervane/variables.sh
@@ -2,5 +2,5 @@
 #run source variables.sh in the directory the app is in to set the variables
 
 export DB_USERNAME=willardscott
-export DB_PASSWORD=twistersux
-export DB_NAME=weather_app_store
+export DB_PASSWORD=alrokersux
+export DB_NAME=weather_app
