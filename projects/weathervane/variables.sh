08875e145d0069296614618f01744c18eda16bf4
soitbros <clint.osterholz@datadoghq.com>
2019-08-15 20:32:21 -0400

    pulls db credentials into environment variables

---
+++ projects/weathervane/variables.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#run source variables.sh in the directory the app is in to set the variables
+
+export DB_USERNAME=willardscott
+export DB_PASSWORD=weatherchaser4lyfe
+export DB_NAME=weather_store
