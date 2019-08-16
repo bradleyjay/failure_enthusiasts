ea3a69d009d4f529814f97349de894a9dcb70bc4
soitbros <clint.osterholz@datadoghq.com>
2019-08-15 20:43:40 -0400

    gitignore is so terrible

--- projects/weathervane/variables.sh
+++ projects/weathervane/variables.sh
@@ -2,5 +2,5 @@
 #run source variables.sh in the directory the app is in to set the variables
 
 export DB_USERNAME=willardscott
-export DB_PASSWORD=alrokersux
-export DB_NAME=weather_app
+export DB_PASSWORD=deathtoroker
+export DB_NAME=weatherapp
