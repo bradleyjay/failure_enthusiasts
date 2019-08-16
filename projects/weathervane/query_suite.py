3f8d791b381bc4f476c4ac4b4878a02ac3408578
bradleyjay <bradleyjshields@gmail.com>
2019-08-15 20:22:20 -0400

    Query suite - contains read from midnight command.

--- projects/weathervane/query_suite.py
+++ projects/weathervane/query_suite.py
@@ -3,12 +3,12 @@ import time
 import datetime
 
 
-start_time = '1564097280'
-end_time = '1564098720'
+# start_time = '1564097280'
+# end_time = '1564098720'
 attributes = ['time', 'summary', 'temperature']
-table = 'actual_weather'
+# table = 'actual_weather'
 
-data = read_data(start_time, end_time, attributes, table)
+# data = read_data(start_time, end_time, attributes, table)
 # print(data.keys())
 # input()
 
@@ -16,7 +16,7 @@ data = read_data(start_time, end_time, attributes, table)
 # for attribute in attributes:
 #     print(data[attribute])
 
-print(data)
+# print(data)
 
 
 # query to now, or query today...something
@@ -39,22 +39,19 @@ def grab_from_predicted(start_time, end_time, attributes):
 # 1) Compare today
 
 def compare_today(start_time, end_time, attributes):
-    # start_time =  today, 00:00
-    # end_time = today, 23:59
 
-    start_time = datetime.datetime.now()
+    start_time = datetime.datetime.combine(datetime.datetime.today(), datetime.time.min)
 
-    # whats the date today
-    today = datetime.date.today()   # 2019-04-13
-
-    # datetime.datetime(2019, 8, 8, 20, 36, 52, 0).timestamp()
-    # -> interpolate and run. maybe parse from datetime.date.today?
-
-
-    # whats the format for converting a time an date to epoch
+    # - datetime.timedelta(days=0)   will give the  date at midnight two days ago
 
     end_time = datetime.datetime.now().timestamp()
 
     actual_data = read_data(start_time, end_time, attributes, 'actual_weather')
     predicted_data = read_data(start_time, end_time, attributes, 'predictive_weather')
     return actual_data, predicted_data
+
+
+
+
+
+
