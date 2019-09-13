c2cd508c256f0de844d77b2b121259a676f7d27c
bradleyjay <bradleyjshields@gmail.com>
2019-09-12 20:55:13 -0400

    query_suite complete

--- projects/weathervane/flask_app_skeleton.py
+++ projects/weathervane/flask_app_skeleton.py
@@ -12,10 +12,12 @@ def hello():
 def hello_name(name):
     return "Hello {}!".format(name)
 
+
 @app.route('/weather_compare')
 def weather_compare():
     query_suite.compare_today_fake()
     return query_suite.compare_today_fake()
+
 
 if __name__ == '__main__':
     app.run()
