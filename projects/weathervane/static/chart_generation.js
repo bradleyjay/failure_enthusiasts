99729b5e79ef0f8fcede4a6d76f975318fa6f6e9
Shelby Parton <parton.shelby@gmail.com>
2019-10-01 21:31:00 -0400

    edit chart generation

--- projects/weathervane/static/chart_generation.js
+++ projects/weathervane/static/chart_generation.js
@@ -1,18 +1,10 @@
 console.log('D3 Time')
 var formatted_actual_data = formatted_actual_data
-var height = 1000
-var width = 1000
+var height = 700
+var width = 700
+var margin = 100
 console.log(formatted_actual_data)
-// console.log(d3.min(formatted_actual_data, (d) => d[0]))
 
-string_data = ["string1", "string2", "string3", "string4"]
-
-numerical_data = [
-    [0, 0]
-    , [1, 1]
-    , [2, 2]
-    , [3, 3]
-]
 
 d3.select("#title")
     .style("font-size", "40px")
@@ -24,45 +16,41 @@ const svg = d3.select("div")
     .attr("height", height)
     .attr("width", width)
 
+svg.append("rect")
+    .attr("height", height - margin)
+    .attr("width", width - margin)
+    .attr("x", margin)
+    .attr("y", 0)
+    .style("border", "solid green 1px")
+
 // render basic line
 var x = d3.scaleLinear()
     .domain([d3.min(formatted_actual_data, (d) => d[0]), d3.max(formatted_actual_data, (d) => d[0])]) // data-scale coordinates of svg
-    .range([0, width]);  // canvas-centric coordinates
+    .range([margin, width]);  // canvas-centric coordinates
 var y = d3.scaleLinear()
-    .domain([d3.min(formatted_actual_data, (d) => d[1]), d3.max(formatted_actual_data, (d) => d[1])])
-    .range([height, 0]);
+    .domain([0, d3.max(formatted_actual_data, (d) => d[1]) + 10])
+    .range([height - margin, 0]);
+
+var x_axis = d3.axisBottom(x)
+
+var y_axis = d3.axisLeft(y)
 
 // draw line
-
-// svg.append("path")
-//     .datum(numerical_data)
-//     .attr("fill", "none")
-//     .attr("stroke", "steelblue")
-//     .attr("stroke-width", 1.5)
-//     .attr("d"
-//         , d3.line()
-//             .x((d) => x(d[0]))
-//             .y((d) => y(d[1]))
-//     )
-
-// svg.append("path")
-//     .datum(retrieved_data)
-//     .attr("fill", "none")
-//     .attr("stroke", "steelblue")
-//     .attr("stroke-width", 1.5)
-//     .attr("d"
-//         , d3.line()
-//             .x((d) => x(d[0]))
-//             .y((d) => y(d[1]))
-//     )
-
 svg.append("path")
     .datum(formatted_actual_data)
     .attr("fill", "none")
-    .attr("stroke", "steelblue")
+    .attr("stroke", "white")
     .attr("stroke-width", 1.5)
     .attr("d"
         , d3.line()
             .x((d) => x(d[0]))
             .y((d) => y(d[1]))
     )
+
+svg.append("g")
+    .attr("transform", "translate("+margin+",0)")
+    .call(y_axis)
+
+svg.append("g")
+    .attr("transform", "translate(0,"+(height-margin)+")")
+    .call(x_axis)
