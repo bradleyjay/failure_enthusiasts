9a46c887c723c36e9c31b9b1685b7789c47d762c
bradleyjay <bradleyjshields@gmail.com>
2019-10-08 19:49:04 -0400

    Plotted TWO lines. Watch out.

--- projects/weathervane/flask_app_skeleton.py
+++ projects/weathervane/flask_app_skeleton.py
@@ -21,21 +21,31 @@ def chart_loader():
     end_time = str(datetime.datetime.now().timestamp())
 
     print('\n\n grab_from_actual  \n\n')
-    data = query_suite.grab_from_actual(
+
+    actual_data = query_suite.grab_from_actual(
         start_time, end_time, ['time', 'temperature'])
 
-    # save data to variable, pass to js
-    
-    formatted_actual_data = []
-    for actual_dat in data['actual_data']:
-        formatted_actual_data.append([float(actual_dat[0]),float(actual_dat[1])])
+    predictive_data = query_suite.grab_from_predictive(
+        start_time, end_time, ['time', 'temperature'])
 
-    # formatted_actual_data = [[0,1], [1,4],[2,2],[4,0]]
+
+    # unpack from dict (JS can't use it) -> list of lists. save data to variable, pass to js
+
+    formatted_actual_data = []
+    formatted_predictive_data = []
+
+    for data in actual_data['actual_data']:
+        formatted_actual_data.append([float(data[0]),float(data[1])])
+
+    for data in predictive_data['predictive_data']:
+        formatted_predictive_data.append([float(data[0]),float(data[1])])
+
+    formatted_data = [formatted_actual_data, formatted_predictive_data]
 
     print('\n\n\n This is d:\n')
-    print(formatted_actual_data)
+    print(formatted_data)
 
-    return render_template('index.html', formatted_actual_data = formatted_actual_data)
+    return render_template('index.html', formatted_data = formatted_data)
 
 
 if __name__ == '__main__':
--- projects/weathervane/static/chart_generation.js
+++ projects/weathervane/static/chart_generation.js
@@ -1,9 +1,9 @@
 console.log('D3 Time')
-var formatted_actual_data = formatted_actual_data
+var formatted_data = formatted_data
 var height = 700
 var width = 700
 var margin = 100
-console.log(formatted_actual_data)
+console.log(formatted_data)
 
 
 d3.select("#title")
@@ -16,41 +16,65 @@ const svg = d3.select("div")
     .attr("height", height)
     .attr("width", width)
 
-svg.append("rect")
-    .attr("height", height - margin)
-    .attr("width", width - margin)
-    .attr("x", margin)
-    .attr("y", 0)
-    .style("border", "solid green 1px")
+// svg.append("rect")
+//     .attr("height", height - margin)
+//     .attr("width", width - margin)
+//     .attr("x", margin)
+//     .attr("y", 0)
+//     .style("border", "solid green 1px")
 
 // render basic line
+var x_min = Math.min(d3.min(formatted_data[0], (d) => d[0]), d3.min(formatted_data[1], (d) => d[0]))
+
+var x_max = Math.max(d3.max(formatted_data[0], (d) => d[0]), d3.max(formatted_data[1], (d) => d[0]))
+
+var y_max = Math.max(d3.max(formatted_data[0], (d) => d[1]), d3.max(formatted_data[1], (d) => d[1]))
+
+// console.log('x_min, x_max, y_max')
+// console.log(x_min)
+// console.log(x_max)
+// console.log(y_max)
+
 var x = d3.scaleLinear()
-    .domain([d3.min(formatted_actual_data, (d) => d[0]), d3.max(formatted_actual_data, (d) => d[0])]) // data-scale coordinates of svg
+    .domain([x_min, x_max]) // data-scale coordinates of svg
     .range([margin, width]);  // canvas-centric coordinates
+
 var y = d3.scaleLinear()
-    .domain([0, d3.max(formatted_actual_data, (d) => d[1]) + 10])
+    .domain([0, y_max + 10])
     .range([height - margin, 0]);
 
 var x_axis = d3.axisBottom(x)
 
 var y_axis = d3.axisLeft(y)
 
-// draw line
-svg.append("path")
-    .datum(formatted_actual_data)
-    .attr("fill", "none")
-    .attr("stroke", "white")
-    .attr("stroke-width", 1.5)
-    .attr("d"
-        , d3.line()
-            .x((d) => x(d[0]))
-            .y((d) => y(d[1]))
-    )
+function addLine(data_to_plot, line_color) {
+    // draw line
+    // console.log('Function running!')
+    // console.log(data_to_plot)
 
+    // console.log('0th element!')
+    // console.log(data_to_plot[0])
+    // console.log('Error definitely incoming:')
+    svg.append("path")
+        .datum(data_to_plot)
+        .attr("fill", "none")
+        .attr("stroke", line_color)
+        .attr("stroke-width", 1.5)
+        .attr("id", (d) => console.log(d[0]))
+        .attr("d"
+            , d3.line()
+                .x((d) => x(d[0]))
+                .y((d) => y(d[1]))
+        )
+}
+
+addLine(formatted_data[0], "red")
+addLine(formatted_data[1], "black")
 svg.append("g")
-    .attr("transform", "translate("+margin+",0)")
+    .attr("transform", "translate(" + margin + ",0)")
     .call(y_axis)
 
 svg.append("g")
-    .attr("transform", "translate(0,"+(height-margin)+")")
+    .attr("transform", "translate(0," + (height - margin) + ")")
     .call(x_axis)
+
