9a361ecf3fc5a6591dc6b71ff542c9f3c2eeab29
bradleyjay <bradleyjshields@gmail.com>
2019-10-08 21:06:39 -0400

    functionizing

--- projects/weathervane/static/chart_generation.js
+++ projects/weathervane/static/chart_generation.js
@@ -3,78 +3,110 @@ var formatted_data = formatted_data
 var height = 700
 var width = 700
 var margin = 100
-console.log(formatted_data)
+// console.log("BEFORE")
+// console.log(formatted_data)
 
+function createLineChart(formatted_data, chart_title, y_axis_label, x_axis_label) {
+    // Multiplying by 1000 since epochs are in milliseconds
 
-d3.select("#title")
-    .style("font-size", "40px")
+    formatted_data[0].forEach((d) => { d[0] = d[0] * 1000; })
+    formatted_data[1].forEach((d) => { d[0] = d[0] * 1000; })
+    // console.log("AFTER")
+    // console.log(formatted_data)
 
-// create svg canvas
-const svg = d3.select("div")
-    .append("svg")
-    .style("border", "solid red 1px")
-    .attr("height", height)
-    .attr("width", width)
+    // sorting predictive data by time
+    formatted_data[1].sort((x, y) => {
+        return d3.ascending(x[0], y[0]);
+    })
 
-// svg.append("rect")
-//     .attr("height", height - margin)
-//     .attr("width", width - margin)
-//     .attr("x", margin)
-//     .attr("y", 0)
-//     .style("border", "solid green 1px")
+    // d3.selectAll("#chart")
+    //     .attr("width", width)
+    //     .style("border", "solid red 1px")
 
-// render basic line
-var x_min = Math.min(d3.min(formatted_data[0], (d) => d[0]), d3.min(formatted_data[1], (d) => d[0]))
+    d3.selectAll("#chart")
+        .append("h2")
+        .text(chart_title)
 
-var x_max = Math.max(d3.max(formatted_data[0], (d) => d[0]), d3.max(formatted_data[1], (d) => d[0]))
+    // create svg canvas
+    const svg = d3.select("div")
+        .append("svg")
+        .attr("height", height)
+        .attr("width", width)
 
-var y_max = Math.max(d3.max(formatted_data[0], (d) => d[1]), d3.max(formatted_data[1], (d) => d[1]))
+    // svg.append("rect")
+    //     .attr("height", height - margin)
+    //     .attr("width", width - margin)
+    //     .attr("x", margin)
+    //     .attr("y", 0)
+    //     .style("border", "solid green 1px")
 
-// console.log('x_min, x_max, y_max')
-// console.log(x_min)
-// console.log(x_max)
-// console.log(y_max)
+    // render basic line
+    var x_min = Math.min(d3.min(formatted_data[0], (d) => d[0]), d3.min(formatted_data[1], (d) => d[0]))
 
-var x = d3.scaleLinear()
-    .domain([x_min, x_max]) // data-scale coordinates of svg
-    .range([margin, width]);  // canvas-centric coordinates
+    var x_max = Math.max(d3.max(formatted_data[0], (d) => d[0]), d3.max(formatted_data[1], (d) => d[0]))
 
-var y = d3.scaleLinear()
-    .domain([0, y_max + 10])
-    .range([height - margin, 0]);
+    var y_max = Math.max(d3.max(formatted_data[0], (d) => d[1]), d3.max(formatted_data[1], (d) => d[1]))
 
-var x_axis = d3.axisBottom(x)
+    // console.log('x_min, x_max, y_max')
+    // console.log(x_min)
+    // console.log(x_max)
+    // console.log(y_max)
 
-var y_axis = d3.axisLeft(y)
+    var x = d3.scaleLinear()
+        .domain([x_min, x_max]) // data-scale coordinates of svg
+        .range([margin, width]);  // canvas-centric coordinates
 
-function addLine(data_to_plot, line_color) {
-    // draw line
-    // console.log('Function running!')
-    // console.log(data_to_plot)
+    var y = d3.scaleLinear()
+        .domain([0, y_max + 10])
+        .range([height - margin, 0]);
 
-    // console.log('0th element!')
-    // console.log(data_to_plot[0])
-    // console.log('Error definitely incoming:')
-    svg.append("path")
-        .datum(data_to_plot)
-        .attr("fill", "none")
-        .attr("stroke", line_color)
-        .attr("stroke-width", 1.5)
-        .attr("id", (d) => console.log(d[0]))
-        .attr("d"
-            , d3.line()
-                .x((d) => x(d[0]))
-                .y((d) => y(d[1]))
-        )
+    var x_axis = d3.axisBottom(x).tickFormat(d3.timeFormat("%H:%M"))
+
+    var y_axis = d3.axisLeft(y)
+
+    function addLine(data_to_plot, line_color) {
+        // draw line
+        // console.log('Function running!')
+        // console.log(data_to_plot)
+
+        // console.log('0th element!')
+        // console.log(data_to_plot[0])
+        // console.log('Error definitely incoming:')
+        svg.append("path")
+            .datum(data_to_plot)
+            .attr("fill", "none")
+            .attr("stroke", line_color)
+            .attr("stroke-width", 1.5)
+            .attr("id", (d) => console.log(d[0]))
+            .attr("d"
+                , d3.line()
+                    .x((d) => x(d[0]))
+                    .y((d) => y(d[1]))
+            )
+    }
+
+    addLine(formatted_data[0], "red")
+    addLine(formatted_data[1], "black")
+    svg.append("g")
+        .attr("transform", "translate(" + margin + ",0)")
+        .call(y_axis)
+
+    svg.append("g")
+        .attr("transform", "translate(0," + (height - margin) + ")")
+        .call(x_axis)
+
+    svg.append("text")
+        .attr("y", margin / 2)
+        .attr("x", (-height + margin) / 2)
+        .style("text-anchor", "middle")
+        .text(y_axis_label)
+        .attr("transform", "rotate(-90)")
+
+    svg.append("text")
+        .attr("y", height - (margin / 2))
+        .attr("x", (margin + width) / 2)
+        .style("text-anchor", "middle")
+        .text(x_axis_label)
 }
 
-addLine(formatted_data[0], "red")
-addLine(formatted_data[1], "black")
-svg.append("g")
-    .attr("transform", "translate(" + margin + ",0)")
-    .call(y_axis)
-
-svg.append("g")
-    .attr("transform", "translate(0," + (height - margin) + ")")
-    .call(x_axis)
-
+createLineChart(formatted_data, "Predictive vs Actual Weather", "Degrees (F)", "Time")
