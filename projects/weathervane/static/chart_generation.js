89e89f4448e5d4a9236b67655e6122a199ae8fcb
Meghan Maloy <meghan.maloy@datadoghq.com>
2019-10-21 19:48:48 -0400

    fixed empty predictive values bug

--- projects/weathervane/static/chart_generation.js
+++ projects/weathervane/static/chart_generation.js
@@ -44,11 +44,27 @@ function createLineChart(formatted_data, chart_title, y_axis_label, x_axis_label
     //     .style("border", "solid green 1px")
 
     // render basic line
-    var x_min = Math.min(d3.min(formatted_data[0], (d) => d[0]), d3.min(formatted_data[1], (d) => d[0]))
 
-    var x_max = Math.max(d3.max(formatted_data[0], (d) => d[0]), d3.max(formatted_data[1], (d) => d[0]))
+    actual_x_min = d3.min(formatted_data[0], (d) => d[0])
+    actual_x_max = d3.max(formatted_data[0], (d) => d[0])
+    actual_y_max = d3.max(formatted_data[0], (d) => d[1])
 
-    var y_max = Math.max(d3.max(formatted_data[0], (d) => d[1]), d3.max(formatted_data[1], (d) => d[1]))
+    // bogus extreme values in case formatted_data isn't available
+    if (formatted_data[1] = []){
+      predictive_x_min = 32529079406000
+      predictive_x_max = -2183589394000
+      predictive_y_max = -5000
+    } else {
+      predictive_x_min = d3.min(formatted_data[1], (d) => d[0])
+      predictive_x_max = d3.max(formatted_data[1], (d) => d[0])
+      predictive_y_max = d3.max(formatted_data[1], (d) => d[1])
+    }
+
+    var x_min = Math.min(actual_x_min, predictive_x_min)
+
+    var x_max = Math.max(actual_x_max, predictive_x_max)
+
+    var y_max = Math.max(actual_y_max, predictive_y_max)
 
     // console.log('x_min, x_max, y_max')
     // console.log(x_min)
@@ -68,6 +84,8 @@ function createLineChart(formatted_data, chart_title, y_axis_label, x_axis_label
     var y_axis = d3.axisLeft(y)
 
     function addLine(data_to_plot, line_color) {
+
+        console.log("Data to Plot")
         // draw line
         // console.log('Function running!')
         // console.log(data_to_plot)
@@ -75,12 +93,13 @@ function createLineChart(formatted_data, chart_title, y_axis_label, x_axis_label
         // console.log('0th element!')
         // console.log(data_to_plot[0])
         // console.log('Error definitely incoming:')
+        console.log(data_to_plot)
         svg.append("path")
             .datum(data_to_plot)
             .attr("fill", "none")
             .attr("stroke", line_color)
             .attr("stroke-width", 1.5)
-            .attr("id", (d) => console.log(d[0]))
+            .attr("id", (d) => console.log("d0: " + x(d[0]) + " d1: " + y(d[1])))
             .attr("d"
                 , d3.line()
                     .x((d) => x(d[0]))
@@ -89,7 +108,7 @@ function createLineChart(formatted_data, chart_title, y_axis_label, x_axis_label
     }
 
     addLine(formatted_data[0], "red")
-    addLine(formatted_data[1], "black")
+    // addLine(formatted_data[1], "black")
     svg.append("g")
         .attr("transform", "translate(" + margin + ",0)")
         .call(y_axis)
