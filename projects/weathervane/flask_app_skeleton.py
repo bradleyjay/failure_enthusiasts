5b6491f8ef46f2596652a26019a709224ad722b7
mikepcarlos <mike.carlos@datadoghq.com>
2020-01-14 21:24:55 -0500

    added /update route to flask skeleton

--- projects/weathervane/flask_app_skeleton.py
+++ projects/weathervane/flask_app_skeleton.py
@@ -3,7 +3,7 @@ import query_suite
 
 import io
 import random
-from flask import Response, render_template
+from flask import Response, render_template, request, redirect
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import query_suite
@@ -11,20 +11,22 @@ import datetime
 
 app = Flask(__name__)
 
+metric = 'temperature'
+date = datetime.datetime.today()
 
 @app.route('/')
-def chart_loader():
-
+def chart_loader(date, metric):
+    #pass date here
     start_time = str(datetime.datetime.combine(
         datetime.datetime.today(), datetime.time.min).timestamp())
-
+    #pass date here
     end_time = str(datetime.datetime.now().timestamp())
 
     print('\n\n grab_from_actual  \n\n')
-
+    #pass metric here
     actual_data = query_suite.grab_from_actual(
         start_time, end_time, ['time', 'temperature'])
-
+        #pass metric here
     predictive_data = query_suite.grab_from_predictive(
         start_time, end_time, ['time', 'temperature'])
     print('Predictive!')
@@ -48,6 +50,12 @@ def chart_loader():
 
     return render_template('index.html', formatted_data = formatted_data)
 
+@app.route('/update', methods=['POST'])
+def update_chart():
+    date = request.form['date']
+    metric_name = request.form['metric_name']
+    # return str(date + ' ' +  metric_name)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run()
