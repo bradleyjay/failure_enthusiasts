b3f2a3960f1704fda91b5b804e8efd1b1979b794
Meghan Maloy <meghan.maloy@datadoghq.com>
2020-01-14 19:51:45 -0500

    restoring previous legend and hover

--- projects/weathervane/static/chart_generation.js
+++ projects/weathervane/static/chart_generation.js
@@ -9,27 +9,29 @@ var width = 700
 var margin = 100
 var legend_width = 200
 var legend_height = 200
-// console.log("BEFORE")
-// console.log(formatted_data)
+var legend_increment = 0
 
-// error here - we assumed two series, predicive data will now only show up one hour into running the agent. SO, need to stop D3 from freaking out when it gets an empty [] instead of an array of arrays with data, for predictive_data specifically.
+var tooltip = d3.select("body")
+    .append("div")
+    .style("position", "absolute")
+    .style("z-index", "10")
+    .style("visibility", "hidden")
+    .style("background", "#000")
+    .style("color", "white")
+    .text("a simple tooltip");
 
-function createLineChart(actual_data, predictive_data, chart_title, y_axis_label, x_axis_label) {
+// this might be fixed table for now - check with proper predictive data. Make sure two lines are graphed. (error here - we assumed two series, predicive data will now only show up one hour into running the agent. SO, need to stop D3 from freaking out when it gets an empty [] instead of an array of arrays with data, for predictive_data specifically.)
+
+function createLineChart(actual_data, predictive_data, chart_title, y_axis_label, x_axis_label, dataset_name) {
     // Multiplying by 1000 since epochs are in milliseconds
 
     actual_data.forEach((d) => { d["time"] = d["time"] * 1000; })
     predictive_data.forEach((d) => { d["time"] = d["time"] * 1000; })
-    // console.log("AFTER")
-    // console.log(formatted_data)
 
     // sorting predictive data by time
     predictive_data.sort((x, y) => {
         return d3.ascending(x[0], y[0]);
     })
-
-    // d3.selectAll("#chart")
-    //     .attr("width", width)
-    //     .style("border", "solid red 1px")
 
     d3.selectAll("#chart")
         .append("h2")
@@ -39,8 +41,6 @@ function createLineChart(actual_data, predictive_data, chart_title, y_axis_label
     const svg = d3.select("#chart")
         .append("svg")
         .attr('viewBox', '0 0 ' + width + ' ' + height);
-        // .attr("height", height)
-        // .attr("width", width)
 
     // legend
     svg.append("g")
@@ -48,13 +48,6 @@ function createLineChart(actual_data, predictive_data, chart_title, y_axis_label
         .attr("transform", "translate(" + (width - legend_width) + ", 0)")
         .attr("height", legend_height)
         .attr("width", legend_width)
-
-    // svg.append("rect")
-    //     .attr("height", height - margin)
-    //     .attr("width", width - margin)
-    //     .attr("x", margin)
-    //     .attr("y", 0)
-    //     .style("border", "solid green 1px")
 
     // render basic line
 
@@ -80,11 +73,6 @@ function createLineChart(actual_data, predictive_data, chart_title, y_axis_label
 
     var y_max = Math.max(actual_y_max, predictive_y_max)
 
-    // console.log('x_min, x_max, y_max')
-    // console.log(x_min)
-    // console.log(x_max)
-    // console.log(y_max)
-
     var x = d3.scaleLinear()
         .domain([x_min, x_max]) // data-scale coordinates of svg
         .range([margin, width]);  // canvas-centric coordinates
@@ -97,17 +85,8 @@ function createLineChart(actual_data, predictive_data, chart_title, y_axis_label
 
     var y_axis = d3.axisLeft(y)
 
-    function addLine(data_to_plot, line_color) {
+    function addLine(data_to_plot, dataset_name, line_color) {
 
-        console.log("Data to Plot")
-        // draw line
-        // console.log('Function running!')
-        // console.log(data_to_plot)
-
-        // console.log('0th element!')
-        // console.log(data_to_plot[0])
-        // console.log('Error definitely incoming:')
-        console.log(data_to_plot)
         svg.append("path")
             .datum(data_to_plot)
             .attr("fill", "none")
@@ -119,28 +98,55 @@ function createLineChart(actual_data, predictive_data, chart_title, y_axis_label
                     .x((d) => x(d["time"]))
                     .y((d) => y(d["temperature"]))
             )
+            .on("mouseover", () => tooltip.style("visibility", "visible"))
+            .on("mousemove", (d) => {
+                tooltip.text(dataset_name + "\nTime: " + d[0] + "\nValue: " + d[1])
+                tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px")
+            })
+            .on("mouseout", () => tooltip.style("visibility", "hidden"))
+
+        dynamic_mod = Math.floor(data_to_plot.length * 0.1)
+
+        svg.selectAll(".dot")
+            .data(data_to_plot)
+            .enter()
+            .append("circle")
+            .attr("r", 3)
+            .attr("cx", d => x(d["time"]))
+            .attr("cy", d => y(d["temperature"]))
+            .attr("fill", (d, i) => {
+                if (i % dynamic_mod == 0) {
+                    return line_color
+                } else {
+                    return "none"
+                }
+            })
 
         d3.select("#legend")
             .append("rect")
+            .attr("transform", "translate(0, " + legend_increment + ")")
             .attr("height", 15)
             .attr("width", 15)
             .attr("fill", line_color)
 
         d3.select("#legend")
             .append('text')
-            .attr("transform", "translate(20,13.5)")
+            .attr("transform", "translate(20," + (13.5 + legend_increment) + ")")
             .attr("height", 30)
             .attr("width", 100)
             .style("font-size", 15)
-            .text("Hi or whatever")
+            .text(dataset_name)
+
+
+        legend_increment += 23
         // Currently, next graph overwrites this block, hence why BLACK shows up, not RED
         // for next time, https://www.d3-graph-gallery.com/graph/custom_legend.html
     }
 
-    addLine(actual_data, "red")
+    addLine(actual_data, dataset_name[0], "red")
 
     if (predictive_data != []) {
-        addLine(predictive_data, "black")
+        addLine(predictive_data, dataset_name[1], "black")
     }
     svg.append("g")
         .attr("transform", "translate(" + margin + ",0)")
@@ -166,4 +172,4 @@ function createLineChart(actual_data, predictive_data, chart_title, y_axis_label
 }
 
 
-createLineChart(actual_data, predictive_data, "Predictive vs Actual Weather", "Degrees (F)", "Time")
+createLineChart(actual_data, predictive_data, "Predictive vs Actual Weather", "Degrees (F)", "Time", ['Actual', 'Predicitive'])
